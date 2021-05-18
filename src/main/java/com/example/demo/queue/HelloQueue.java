0a4ac0e1226698bb422f52dc5cc8b24afad6d921
yunyubowz <1196630013@qq.com>
2021-05-18 23:59:08 +0800

    队列

--- src/main/java/com/example/demo/queue/HelloQueue.java
+++ src/main/java/com/example/demo/queue/HelloQueue.java
@@ -8,11 +8,18 @@ public class HelloQueue {
     public static void main(String[] args) {
         Queue<String> queue = new ArrayBlockingQueue<>(6);
         queue.add("5");
+        System.out.println(queue.peek());
+
         queue.add("6");
         queue.add("1");
         queue.add("2");
         queue.add("3");
         queue.add("4");
-        System.out.println(queue);
+        for(int i = 0;i<6;i++){
+            System.out.println("element--->"+queue.element());
+            System.out.println(queue.poll());
+
+        }
+
     }
 }
